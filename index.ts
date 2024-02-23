dac6c8046ee70a4240bb29dcdd613da17db4a0a4
thanh6886 <phanquangthanh136@gmail.com>
2024-02-23 23:40:08 +0700

    .

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import userRoutes from './routes/user/index.route'
 import { responseError } from './utils/response'
 import { FOLDERS, FOLDER_UPLOAD, ROUTE_IMAGE } from './constants/config'
 import path from 'path'
-import { isProduction } from './utils/helper'
+import { handlerImage } from './utils/helper'
 require('dotenv').config()
 
 const app: express.Application = express()
@@ -18,18 +18,6 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-const dirNameWithEnv = isProduction ? path.dirname(__dirname) : __dirname
-
-const handlerImage: any = Object.values(FOLDERS).reduce(
-  (result: any, current: any) => {
-    return [
-      ...result,
-      express.static(path.join(dirNameWithEnv, `/${FOLDER_UPLOAD}/${current}`)),
-    ]
-  },
-  [express.static(path.join(dirNameWithEnv, `/${FOLDER_UPLOAD}`))]
-)
 
 app.use(`/${ROUTE_IMAGE}`, ...handlerImage)
 
--- utils/helper.ts
+++ utils/helper.ts
@@ -1,11 +1,13 @@
+import path from 'path'
+import { FOLDERS, FOLDER_UPLOAD } from '../constants/config'
+import express from 'express'
+
 require('dotenv').config()
 
-export const isProduction =
+const isProduction =
   process.env.NODE_ENV === 'production' || process.argv[2] === 'production'
 
-export const HOST = isProduction
-  ? process.env.PRODUCTION_HOST
-  : `http://${process.env.HOST}:${process.env.PORT}`
+export const HOST = `http://localhost:${process.env.PORT}`
 
 export function removeAccents(str) {
   return str
@@ -14,3 +16,14 @@ export function removeAccents(str) {
     .replace(/đ/g, 'd')
     .replace(/Đ/g, 'D')
 }
+const dirNameWithEnv = isProduction ? path.dirname(__dirname) : __dirname
+
+export const handlerImage: any = Object.values(FOLDERS).reduce(
+  (result: any, current: any) => {
+    return [
+      ...result,
+      express.static(path.join(dirNameWithEnv, `/${FOLDER_UPLOAD}/${current}`)),
+    ]
+  },
+  [express.static(path.join(dirNameWithEnv, `/${FOLDER_UPLOAD}`))]
+)
