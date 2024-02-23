348c193ba2c35ecaaf79a7affa40ffc1cda29c76
thanh6886 <phanquangthanh136@gmail.com>
2024-02-23 23:15:44 +0700

    .

--- constants/config.ts
+++ constants/config.ts
@@ -4,7 +4,6 @@ export const config = {
   EXPIRE_ACCESS_TOKEN: 7 * 24 * 60 * 60,
   EXPIRE_REFRESH_TOKEN: 100 * 24 * 60 * 60,
 }
-// 60, "2 days", "10h", "7d". A numeric value is interpreted as a seconds count. If you use a string be sure you provide the time units (days, hours, etc), otherwise milliseconds unit is used by default ("120" is equal to "120ms").
 
 export const FOLDER_UPLOAD = 'upload'
 
--- constants/role.enum.ts
+++ constants/role.enum.ts
@@ -1,4 +1,3 @@
 export const ROLE = {
-  // ADMIN: 'Admin',
   USER: 'User',
 }
