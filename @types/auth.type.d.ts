b35f858f1d97821d2edeaf119f92da4fe797725c
thanh6886 <phanquangthanh136@gmail.com>
2024-02-23 23:44:20 +0700

    .

--- @types/auth.type.d.ts
+++ @types/auth.type.d.ts
@@ -1,21 +1,20 @@
-interface Register {
-  email: string
-  password: string
-  name: string,
-  date_of_birth: string,
-  address: string,
-  phone: string
-}
-
 interface Login {
   email: string
   password: string
 }
+interface Register {
+  email: string
+  password: string
+  name: string
+  date_of_birth: string
+  address: string
+  phone: string
+}
 
 interface PayloadToken {
-  id: string,
-  email: string,
-  roles: string[],
+  id: string
+  email: string
+  roles: string[]
   created_at: string
 }
 
