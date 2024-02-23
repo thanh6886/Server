933dd6bdc1977bcb803ad7b85e027e9180925d8c
thanh6886 <phanquangthanh136@gmail.com>
2024-02-23 23:45:26 +0700

    .

--- @types/user.type.d.ts
+++ @types/user.type.d.ts
@@ -1,11 +1,11 @@
 interface User {
-  email: string,
-  password: string,
+  email: string
+  password: string
   name: string
-  date_of_birth: string,
-  address: string,
-  phone: string,
-  roles: string[],
-  avatar?: string,
+  address: string
+  phone: string
+  roles: string[]
+  date_of_birth: string
+  avatar?: string
   [key: string]: any
 }
