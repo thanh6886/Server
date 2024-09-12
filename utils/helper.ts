c75b37be52ed1f29509a74d3c2dbd02b224c17e8
Phan Quang Thành <phanquangthanh136@gmail.com>
2024-09-12 23:51:15 +0700

    .

--- utils/helper.ts
+++ utils/helper.ts
@@ -7,8 +7,9 @@ require('dotenv').config()
 export const isProduction =
   process.env.NODE_ENV === 'production' || process.argv[2] === 'production'
 
-export const HOST = `http://${process.env.HOST}:${process.env.PORT}`
-
+export const HOST = isProduction
+  ? process.env.PRODUCTION_HOST
+  : `http://${process.env.HOST}:${process.env.PORT}`
 export function removeAccents(str) {
   return str
     .normalize('NFD')
